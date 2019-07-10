4491fe8bf64e453d01be3b8b8b19ce852ebae236
azu-ref <fjah17@gmail.com>
2019-07-10 19:46:13 -0400

    Proyecto del curso

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: '472106db668997ded909afaa9186c170',
+    secret: '72e66272e1de40d0d60fe14bec87ffee',
+    registeredTo: 'azu-ref'
+}
+
+// Application name 	platzimusic
+// API key 	472106db668997ded909afaa9186c170
+// Shared secret 	72e66272e1de40d0d60fe14bec87ffee
+// Registered to 	azu-ref
